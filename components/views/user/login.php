134d440b5fa44da273a9ce1e1f044edc4e693b9b
Théo Morin Vasseur <theo.theo-morin-vasseur@etu.u-bordeaux.fr>
2020-12-03 23:38:41 +0100

    Users views

---
+++ components/views/user/login.php
@@ -0,0 +1,48 @@
+<div class="container">
+    <div class="row">
+        <div class="my-card login-form">
+            <div class="choose">
+                <div id="choose-signin">Sign in</div>
+                <div id="choose-signup">Sign up</div>
+            </div>
+            <div id="signin">
+                <h1>Sign in</h1>
+                <form method="POST">
+                    <div class="form-group">
+                        <input type="text" name="login" class="form-control" placeholder="Login (email address)">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="passwd" class="form-control" placeholder="Password">
+                    </div>
+                    <div>
+                        <button class="btn btn-primary float-right">Connexion</button>
+                    </div>
+                </form>
+            </div>
+            <div id="signup">
+                <h1>Sign up</h1>
+                <form method="POST">
+                    <div class="form-group">
+                        <input type="email" name="email" class="form-control" placeholder="Email address">
+                    </div>
+                    <div class="form-group">
+                        <input type="text" name="fullname" class="form-control" placeholder="Fullname">
+                    </div>
+                    <div class="form-group">
+                        <div class="row">
+                            <div class="col">
+                                <input type="password" name="passwd" class="form-control" placeholder="Password">
+                            </div>
+                            <div class="col">
+                                <input type="password" name="confpasswd" class="form-control" placeholder="Confirm password">
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <button class="btn btn-primary float-right">Inscription</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
